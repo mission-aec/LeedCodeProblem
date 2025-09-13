2c3d4693d7d223e57b59a3456f53b5c819e7a2c1
missiom-aec <missiomhassan35@gamil.com>
2025-09-13 14:08:42 +0600

    leed code problem 14

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,50 +1,31 @@
-/* 14. Longest Common Prefix
-Write a function to find the longest common prefix string amongst an array of strings.
-
-If there is no common prefix, return an empty string "".
-
- 
-
-Example 1:
-
-Input: strs = ["flower","flow","flight"]
-Output: "fl"
-Example 2:
-
-Input: strs = ["dog","racecar","car"]
-Output: ""
-Explanation: There is no common prefix among the input strings.
- 
-
-Constraints:
-
-1 <= strs.length <= 200
-0 <= strs[i].length <= 200
-strs[i] consists of only lowercase English letters if it is non-empty.
-*/
 package ProblemSolving;
 
-class longestCommonPrefix{
-    
-    public static char longestCommonPrefix (String[] strs){
-        //System.out.println(strs.length);
-        //System.out.println(strs[2].length());
-        for(int i=0 ; i < strs.length; i++){
-           for(int j=0 ; j < strs[i].length(); j++){
-           if(strs[i].charAt(j)==strs[i+1].charAt(j) && strs[i+1].charAt(j)==strs[i+2].charAt(j)){
-               return strs[i].charAt(j);
-           }else{
-               return ' ';
-           }  
-        
-       }    
-       }
-    return ' ';
+public class LongestCommonPrefix {
 
-    }
-    public static void main(String []args){
-       String[] Strs ={"flower","flow","flight"}; 
-       System.out.print("\"" + longestCommonPrefix(Strs) + "\""); 
-       //System.out.println(Strs[0].charAt(1));
+ public static String longestCommonPrefix(String[] strs) {
+        String s=strs[0];
+
+        for(byte i=0; i<strs.length; i++){
+            if(strs[i].length()<s.length()){
+                s=s.substring(0,strs[i].length());
+            }
+            else if(strs[i].length()==0){
+                s="";
+                return s;
+            }
+            for(byte j=0; j<Math.min(strs[i].length(),s.length()); j++){
+                if(strs[i].charAt(j)!=s.charAt(j)){
+                    s=s.substring(0,j);
+                }
+            }
+        }
+        return s;
+        
+}
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        String s=longestCommonPrefix(strs);
+        System.out.println(s);
     }
 }
